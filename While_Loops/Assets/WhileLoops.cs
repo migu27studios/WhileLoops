1bbf49ae1e02565561b20f084df9774e83c4f3ab
Unknown <migu27studios@gmail.com>
2017-02-15 10:49:01 -0700

    While Loops

---
+++ While_Loops/Assets/WhileLoops.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoops : MonoBehaviour {
+
+    int i = 0;    //Iterator = Something that can increase or decrease
+
+
+    /*
+	// Use this for initialization
+	void Start () {
+        while (i < 10)
+        {
+            print(i);
+            i++;
+        }
+        print("Done! :D");*/
+
+
+    public string[] colors;
+
+        void Start () {
+            while (i < colors.Length)
+            {
+                print(colors[i]);
+                i++;
+            }
+
+
+            print("Done! :D");
+
+
+
+        }
+}
